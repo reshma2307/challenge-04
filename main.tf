38fac2ca91e8639878dcb4b0a20ff99a4b2c2554
reshma2307 <fathimareshma250@gmail.com>
2024-12-18 16:41:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,52 +1,62 @@
 provider "aws" {
   region = "ap-northeast-2"
 }
-
-resource "aws_instance" "c8" {
-  ami           = "ami-049788618f07e189d"  
-  instance_type = "t2.micro"
-  key_name      = "ansible"  
-  
-  tags = {
-    Name = "c8.local"
-  }
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo hostnamectl set-hostname c8.local
-  EOF
-}
-
-resource "aws_instance" "u21" {
-  ami           = "ami-0dc44556af6f78a7b"  
-  instance_type = "t2.micro"
-  key_name      = "ansible"  
-  
+resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
+  ami                    = "ami-0dc44556af6f78a7b"
+  instance_type          = "t2.micro" 
+  key_name               = "ansible"
+  vpc_security_group_ids = ["sg-084c24c6eb8ddcad3"]
   tags = {
     Name = "u21.local"
   }
-
   user_data = <<-EOF
-    #!/bin/bash
-    sudo hostnamectl set-hostname u21.local
-  EOF
-}
-
-output "c8_public_ip" {
-  value = aws_instance.c8.public_ip
-}
-
-output "u21_public_ip" {
-  value = aws_instance.u21.public_ip
-}
-
-resource "local_file" "ansible_inventory" {
-  content = <<EOF
-  filename = "./inventory.yaml"
-[frontend]
-${aws_instance.c8.public_ip} 
-
-[backend]
-${aws_instance.u21.public_ip} 
+  #!/bin/bash
+  sudo hostnamectl set-hostname U21.local
+  # netdata_conf="/etc/netdata/netdata.conf"
+  # Path to netdata.conf
+  # actual_ip=0.0.0.0
+  # Use sed to replace the IP address in netdata.conf
+  # sudo sed -i "s/bind socket to IP = .*$/bind socket to IP = $actual_ip/" "$netdata_conf"
 EOF
+
+}
+
+resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
+  ami                    = "ami-049788618f07e189d"
+  instance_type          = "t2.micro"
+  key_name               = "ansible"
+  vpc_security_group_ids = ["sg-084c24c6eb8ddcad3"]
+  tags = {
+    Name = "c8.local"
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  # New hostname and IP address
+  sudo hostnamectl set-hostname c8.local
+  hostname=$(hostname)
+  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
+
+  # Path to /etc/hosts
+  echo "${aws_instance.backend.public_ip} $hostname" | sudo tee -a /etc/hosts
+
+EOF
+depends_on = [aws_instance.backend]
+}
+
+resource "local_file" "inventory" {
+  filename = "./inventory.yaml"
+  content  = <<EOF
+[frontend]
+${aws_instance.frontend.public_ip}
+[backend]
+${aws_instance.backend.public_ip}
+EOF
+}
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
 }
